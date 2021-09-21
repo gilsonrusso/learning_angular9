fc7ccec9824aac5105af960dbf039a60d75e7d3d
Gilson Russo <gilsonrusso@outlook.com>
2021-09-21 19:29:27 -0400

    Create the form to add produts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,10 @@ import { MatCardModule } from "@angular/material/card";
 import { MatButtonModule } from "@angular/material/button";
 import { MatSnackBarModule } from "@angular/material/snack-bar"
 
+import { MatFormFieldModule } from "@angular/material/form-field"
+import { MatInputModule } from "@angular/material/input"
+import { FormsModule } from "@angular/forms"
+
 import { HttpClientModule } from "@angular/common/http";
 
 // Import components
@@ -48,7 +52,10 @@ import { ForDirective } from "./directives/for.directive";
     MatCardModule,
     MatButtonModule,
     MatSnackBarModule,
-    HttpClientModule
+    HttpClientModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent],
