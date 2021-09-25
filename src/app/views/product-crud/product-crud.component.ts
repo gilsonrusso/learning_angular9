153f58fe834a4be4816ed0f31c50cd04dd025db2
Gilson Russo <gilsonrusso@outlook.com>
2021-09-25 12:11:34 -0400

    comunications between components header

--- src/app/views/product-crud/product-crud.component.ts
+++ src/app/views/product-crud/product-crud.component.ts
@@ -1,3 +1,4 @@
+import { HeaderService } from "./../../components/template/header/header.service";
 import { Component, OnInit } from "@angular/core";
 import { Router } from "@angular/router";
 
@@ -7,7 +8,13 @@ import { Router } from "@angular/router";
   styleUrls: ["./product-crud.component.css"],
 })
 export class ProductCrudComponent implements OnInit {
-  constructor(private router: Router) {}
+  constructor(private router: Router, private headerServices: HeaderService) {
+    headerServices.headerData = {
+      title: "Cadastro de Produtos",
+      icon: "store",
+      routerUrl: "/products",
+    };
+  }
 
   ngOnInit(): void {}
 
